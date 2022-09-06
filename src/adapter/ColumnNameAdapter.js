8cfa655295966da9d44bcf03c59f8ff412944154
Amit Chavda <amitchavda9879@gmail.com>
2022-09-06 13:23:01 +0530

    Created ColumnNameAdapter

---
+++ src/adapter/ColumnNameAdapter.js
@@ -0,0 +1,18 @@
+const columnNames = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+
+    {
+        field: 'fullName',
+        headerName: 'Full name',
+        description: 'This column has a value getter and is not sortable.',
+        sortable: false,
+        width: 160,
+        valueGetter: (params) =>
+            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+    { field: 'email', headerName: 'Email', width: 160, },
+    { field: 'address', headerName: 'Address', width: 200, },
+];
+export default columnNames
